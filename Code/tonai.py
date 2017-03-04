ddaa7099d8dc7d1cb1218023232b6acf9f5bd173
ozzstrich <orahmab@essex.ac.uk>
2017-03-04 17:32:02 +0000

    Added more prefixes and slight tweaks to natural language

--- Code/tonai.py
+++ Code/tonai.py
@@ -1,35 +1,35 @@
 # Osama Rahman 1304349 Open Domain QA System - TonAI
-# Why can't I get birthYear for Adolf Hitler?
-# DBpedia note: If answer returns link, get end of URL for answer.
-# IE: "Who was the cinematographer the Westworld TV show?"
-# Would return a link to Paul Cameron's page
-# Instead, get "/Paul_Cameron_" from URL
+# If there is no English translation, system won't return anything
+# TODO Make use of other prefixes to get more information - Need more than just comment
 
 from SPARQLWrapper import SPARQLWrapper, JSON
-import sys
 import nltk
-from nltk.corpus import brown
 
 print '\n', "Hi, I'm TonAI. What would you like to know about? "
 
 inp = raw_input()
 inp_string = inp
-# response = inp.replace(" ", "_")
 
+# Use for quick test - when you don't want to use other inputs
+# response = inp.replace(" ", "_")
 # print '\n', "Enter a prefix (rdfs, dbo)"
 # prefix_input = raw_input()
 # prefix = prefix_input
-#
 # print '\n', "Enter a property input (comment, birthYear)"
 # property_input = raw_input()
 # label = property_input
-
-# Use for quick test - when you don't want to use other inputs
 
 inp_tokens = nltk.word_tokenize(inp)
 tagger = nltk.pos_tag
 tagged_input = tagger(inp_tokens)
 print tagged_input
+
+# Tags to be used to identify keywords for response
+# Change as you go along, testing different URLs
+# TODO if 'IN' comes before NNP(and maybe others) add to response
+response_tags = ['NNP', 'NN', 'JJ', 'NNS', 'IN']
+
+
 prefix = ""
 label = "comment"
 response = ""
@@ -42,23 +42,19 @@ for i in tagged_input:
 
 temp = []
 for i in tagged_input:
-    if 'NNP' in i:
-        temp.append(i)
+    if i[1] in response_tags:
+        temp.append(i[0])
 
-response = temp[0]
-print "response: ", response
+response = "_".join(temp)
 
-print len(inp_tokens)
-
-if len(inp_tokens) > 3:
-    response = inp_tokens[-3]+"_"+inp_tokens[-2]
-
-if len(inp_tokens) < 3:
-    response = inp.replace(" ", "_")
+if "?" not in inp:
+    response.replace(" ", "_")
+    prefix = "rdfs"
 
 
-print response
-# print inp_tokens[-3], inp_tokens[-2]
+print "response:", response
+print "temp: ", temp
+
 
 print '\n', "http://dbpedia.org/resource/"+response+">"+prefix+":"+label+"?"+label
 
@@ -71,8 +67,11 @@ def info_retrieval():
 
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
         PREFIX dbo: <http://dbpedia.org/ontology/>
-
+        PREFIX dbp: <http://dbpedia.org/property/>
+        PREFIX dct: <http://purl.org/dc/terms/>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 
         SELECT ?"""+label+"""
         WHERE { <http://dbpedia.org/resource/"""+response+""">"""+prefix+""":"""+label+""" ?"""+label+"""
